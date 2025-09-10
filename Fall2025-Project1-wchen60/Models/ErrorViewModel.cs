e70cc82a11b4cd6890dc7304d1ab3a4774a7c38e
Warren Chen <warrenchen2406@gmail.com>
2025-09-10 16:52:28 -0500

    Initial Commit Project 1

---
+++ Fall2025-Project1-wchen60/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2025_Project1_wchen60.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
